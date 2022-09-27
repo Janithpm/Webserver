0c30f2bc7a1408f0a2424cb615ed574d93b316fa
Janith Madarasinghe <janithpm9991@gmail.com>
2022-09-27 10:52:35 +0530

    create simple website

--- htdocs/js/index.js
+++ htdocs/js/index.js
@@ -1,2 +1,1 @@
-const box = document.querySelector('.box')
-TweenMax.to(box, 3, {rotation:"360", ease:Linear.easeNone, repeat:-1});
+document.getElementById("btn").addEventListener("click", () => alert("This is a alert message!"))
